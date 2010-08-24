cc6272ea6e7645efacd2f2190f615ec696d17471
Gregory McWhirter <greg@hallofkvasir.org>
2010-08-24 16:14:19 -0700

    Removing a lot of things not needed since we won't be using the full couchapp experience

---
+++ update_design_docs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "You must supply a database"
+	exit
+fi
+
+cd sitedb
+for directory in *
+do
+	echo -n "${directory}"
+
+	if [ "${directory}" == "." ]
+	then
+		echo "... skipping ."
+		continue
+	fi
+
+	if [ "${directory}" == ".." ]
+	then
+		echo "... skipping .."
+		continue
+	fi
+
+	if [ -d ./${directory} ]
+	then
+		echo "... processing"
+		couchapp push ${directory} $1
+	else
+		echo "... skipping non-directory"
+		continue
+	fi
+done
