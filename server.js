b5bd660fee6b15ceaf3471a89cbddcc5ab55739b
Gregory McWhirter <greg@hallofkvasir.org>
2010-08-29 20:40:30 -0700

    Adding some more general stuff

--- server.js
+++ server.js
@@ -13,43 +13,42 @@ var config = {
 	server_port: 80
 };
 
-web_server = connect.createServer(
-    connect.logger(), //log to terminal
-    connect.conditionalGet(), //adds not-modified support
-    connect.cache(), //adds caching
-    connect.gzip(), //compresses various content type responses
-    connect.cookieDecoder(), //populates req.cookies
-    connect.router(require('./lib/default').urls('')),
-    connect.router(require('./lib/club').urls('/club')),
-    connect.router(require('./lib/forum').urls('/forum')),
-    connect.router(require('./lib/game').urls('/game')),
-    connect.router(require('./lib/news').urls('/news')),
-    connect.router(require('./lib/player').urls('/player')),
-    connect.compiler({src: __dirname + "/media/css", enable: ["less"]}), //compiles less files into css to serve statically
-    connect.staticProvider(__dirname + "/media") //serve static files in the media directory
-);
+connect.cache(100000);
 
-if (config.use_ssl)
-{
-	var pkey = fs.readFileSync(config.ssl_pkey).toString();
-	var cert = fs.readFileSync(config.ssl_cert).toString();
-	config.ssl_creds = crypto.createCredentials({key: pkey, cert: cert});
-	ssl_server = connect.createServer(
-		connect.logger(),
-		connect.conditionalGet(),
-		connect.cache(),
-		connect.gzip(),
-		connect.cookieDecoder(),
-		connect.router(require('./lib/default').sslurls('')),
-		connect.router(require('./lib/club').sslurls('/club')),
-		connect.router(require('./lib/forum').sslurls('/forum')),
-		connect.router(require('./lib/game').sslurls('/game')),
-		connect.router(require('./lib/news').sslurls('/news')),
-		connect.router(require('./lib/player').sslurls('/player')),
+var server = function (ssl){
+	ssl = ssl | false;
+	var s = connect.createServer(
+		connect.logger(), //log to terminal
+		connect.conditionalGet(), //adds not-modified support
+		connect.cache(), //adds caching
+		connect.gzip(), //compresses various content type responses
+		connect.cookieDecoder(), //populates req.cookies
+		connect.router(require('./lib/default').urls(ssl, '')),
+		connect.router(require('./lib/club').urls(ssl, '/club')),
+		connect.router(require('./lib/forum').urls(ssl, '/forum')),
+		connect.router(require('./lib/game').urls(ssl, '/game')),
+		connect.router(require('./lib/news').urls(ssl, '/news')),
+		connect.router(require('./lib/player').urls(ssl, '/player')),
 		connect.compiler({src: __dirname + "/media/css", enable: ["less"]}), //compiles less files into css to serve statically
 		connect.staticProvider(__dirname + "/media") //serve static files in the media directory
 	);
-	ssl_server.setSecure(config.ssl_creds);
+	
+	if (ssl)
+	{
+		var pkey = fs.readFileSync(config.ssl_pkey).toString();
+		var cert = fs.readFileSync(config.ssl_cert).toString();
+		var ssl_creds = crypto.createCredentials({key: pkey, cert: cert});
+		s.setSecure(ssl_creds);
+	}
+	
+	return s;
+}
+
+web_server = server(false);
+
+if (config.use_ssl)
+{
+	ssl_server = server(true);
 }
 
 if (config.use_vhosts)
