803e1f9d661a03e76a3515b1f6eb5add067bd95c
Gregory McWhirter <greg@hallofkvasir.org>
2010-09-08 12:43:09 -0700

    Porting everything over to express instead of raw connect

--- server.js
+++ server.js
@@ -1,55 +1,105 @@
-var connect = require('connect'),
-    form = require('connect-form'),
-    mw = require('./lib/base').middleware;
+var express = require('express'),
+    base = require('./lib/base');
+var sys = require('sys');
+    
+var mw = base.middleware,
+    config = base.config;    
     
 var servers = {};
-    
-var config = require('./lib/base').config;
 
-connect.cache(100000);
+var config = function (ssl, env){
+    return {
+        is_ssl: ssl,
+        server_port: ssl ? 7443 : 7080,
+        couchdb: 'http://localhost:5984/node_playground',
+        login_cookie: 'NPLogin',
+        recaptcha_keys: {
+            "djo-dev.org": {"public": "6LcNrroSAAAAAF3Q8ELHO2IiHDZy1IISNbOjP3ll", "private": "6LcNrroSAAAAAMuecSF1QKmoh5xFSR6_048kV8dG"},
+            "evogames.org": {"public": "6Lc15bwSAAAAAFzr7cwwiY-RcqcybDdU9SArAXWa", "private": "6Lc15bwSAAAAAFCBNv4_mYhTj73DzaVwxgc1YqDW"}
+        }
+    };
+};
 
 var server = function (ssl){
 	ssl = ssl || false;
-	var s = connect.createServer(
-		connect.logger(), //log to terminal
-		connect.conditionalGet(), //adds not-modified support
-		connect.cache(), //adds caching
-		connect.gzip(), //compresses various content type responses
-		connect.cookieDecoder(), //populates req.cookies
-		form({keepExtensions: true}),
-		mw.determineLogin(),
-		connect.compiler({src: __dirname + "/media/css", enable: ["less"]}), //compiles less files into css to serve statically
-		connect.router(require('./lib/default').urls(ssl, '')),
-		connect.router(require('./lib/club').urls(ssl, '/club')),
-		connect.router(require('./lib/forum').urls(ssl, '/forum')),
-		connect.router(require('./lib/game').urls(ssl, '/game')),
-		connect.router(require('./lib/news').urls(ssl, '/news')),
-		connect.router(require('./lib/player').urls(ssl, '/player')),
-		connect.router(require('./lib/api').urls(ssl, '/api')),
-		connect.staticProvider(__dirname + "/media"), //serve static files in the media directory
-		ssl ? mw.forceNonSSL() : mw.nice404()
-	);
+	var app = express.createServer();
 	
-	if (ssl && !config.fake_ssl)
-	{
-	    var fs = require('fs'),
-	        crypto = require('crypto');
+	app.configure(function (){
+	    app.use(express.logger());
+	    app.use(express.gzip());
+	    app.use(express.conditionalGet());
+	    app.use(express.cookieDecoder());
+	    app.use(express.bodyDecoder());
+	    app.use(mw.determineLogin());
+	    app.use(mw.prepareMenus());
+	    app.use(express.cache());
+	    app.use(app.router);
 	    
-		var pkey = fs.readFileSync(config.ssl_pkey).toString();
-		var cert = fs.readFileSync(config.ssl_cert).toString();
-		var ssl_creds = crypto.createCredentials({key: pkey, cert: cert});
-		s.setSecure(ssl_creds);
-	}
+	    app.set('views', __dirname+"/views");
+	    app.set('partials', __dirname+"/views");
+	    app.set('view engine', 'jade');
+	    app.set('view options', {layout: ssl ? 'layout_ssl' : 'layout'});
+	    
+	    app.error(base.handle404);
+	    app.error(base.handle500);
+	    
+	    app.helpers({
+            system: {
+                title: 'EvoGames: Event Verification of Games',
+                name: 'EvoGames',
+                copyright: '&copy; Gregory McWhirter 2010',
+                description: '',
+                keywords: '',
+                default_avatar: '/images/default_avatar.gif',
+                analytics: '',
+                error_email: 'greg@evogames.org',
+                is_ssl: ssl  
+            },
+            menu_list: false
+        });
+        
+        app.dynamicHelpers({
+            player: function (req, res){
+                return req.player;
+            },
+            menus: function (req, res){
+                var menus = new req.Menus();
+                return function (menu_list){
+                    return menus.get(menu_list);
+                };
+            }
+        });
+	});
 	
-	return s;
+	app.configure('development', function (){
+	    app.use(express.compiler({src: __dirname + "/media/css", enable: ["less"]}));
+	    app.use(express.staticProvider(__dirname + "/media"));
+	    app.use(ssl ? mw.forceNonSSL() : mw.nice404());
+	    //app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	    
+	    app.set('config', config(ssl, 'development'));
+	});
+	
+	app.configure('production', function (){
+	    app.use(ssl ? mw.forceNonSSL() : mw.nice404());
+	    
+	    app.set('config', config(ssl, 'production'));
+	});
+		
+	require('./lib/default').urls(ssl, '')(app);
+	require('./lib/club').urls(ssl, '/club')(app);
+	require('./lib/forum').urls(ssl, '/forum')(app);
+	require('./lib/game').urls(ssl, '/game')(app);
+	require('./lib/news').urls(ssl, '/news')(app);
+	require('./lib/player').urls(ssl, '/player')(app);
+	require('./lib/api').urls(ssl, '/api')(app);
+	
+	return app;
 }
 
 servers.web = server(false);
-servers.web.listen(config.server_port);
+servers.web.listen(servers.web.set('config').server_port);
 
-if (config.use_ssl)
-{
-    servers.ssl = server(true);
-    servers.ssl.listen(config.ssl_port);
-}
+servers.ssl = server(true);
+servers.ssl.listen(servers.ssl.set('config').server_port);
 
