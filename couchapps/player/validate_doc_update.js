d9333011a8c14b431af9d7547b637433f0fb1d5d
Gregory McWhirter <greg@hallofkvasir.org>
2010-09-11 09:11:33 -0700

    Making logout work and moving things around to be a bit clearer

--- couchapps/player/validate_doc_update.js
+++ couchapps/player/validate_doc_update.js
@@ -2,27 +2,31 @@ function (newDoc, oldDoc, cdbuser)
 {
     // !code ../validate_helpers.js
     
-    if (newDoc.created_at || (oldDoc && oldDoc.created_at))
+    if(!newDoc._deleted)
     {
-        require((!oldDoc || oldDoc.created_at == newDoc.created_at),
-                'You may not change the creation timestamp.');
-    }
-
-    if (newDoc.type == 'player')
-    {
-        require((!oldDoc || oldDoc.type == newDoc.type),
-                'You may not change the type.');
-        require((equals(newDoc.pending_email_change, {}) || 
-                newDoc.pending_email_change.email.match(email_regex)),
-                'Pending e-mail is not valid.');
-        require(!newDoc.email_history.length || newDoc.email_history[newDoc.email_history.length - 1].email.match(email_regex),
-                'The e-mail address is not valid.');
-        require((!oldDoc || preserve_history(newDoc.email_history, oldDoc.email_history)),
-                'You must preserve email history.');
-        require((!oldDoc || oldDoc.handle == newDoc.handle),
-                'You must not change the handle.');
-    }
     
-    if (newDoc.type == 'login_token')
-    { }
+        if (newDoc.created_at || (oldDoc && oldDoc.created_at))
+        {
+            require((!oldDoc || oldDoc.created_at == newDoc.created_at),
+                    'You may not change the creation timestamp.');
+        }
+    
+        if (newDoc.type == 'player')
+        {
+            require((!oldDoc || oldDoc.type == newDoc.type),
+                    'You may not change the type.');
+            require((equals(newDoc.pending_email_change, {}) || 
+                    newDoc.pending_email_change.email.match(email_regex)),
+                    'Pending e-mail is not valid.');
+            require(!newDoc.email_history.length || newDoc.email_history[newDoc.email_history.length - 1].email.match(email_regex),
+                    'The e-mail address is not valid.');
+            require((!oldDoc || preserve_history(newDoc.email_history, oldDoc.email_history)),
+                    'You must preserve email history.');
+            require((!oldDoc || oldDoc.handle == newDoc.handle),
+                    'You must not change the handle.');
+        }
+        
+        if (newDoc.type == 'login_token')
+        { }
+    }
 }
