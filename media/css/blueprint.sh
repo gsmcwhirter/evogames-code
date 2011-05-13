99a94d59f518467e06040e119b00972fcd454c00
Gregory McWhirter <greg@hallofkvasir.org>
2011-05-13 13:32:08 -0700

    Adding some player profile content

--- media/css/blueprint.sh
+++ media/css/blueprint.sh
@@ -4,6 +4,7 @@ mydir=`dirname $0`
 if cd $mydir
 then
     echo "Generating Blueprint..."
+    lessc evogames.less evogames.css
     ruby ../../../blueprint-1.0/lib/compress.rb -p evogames --settings_file=blueprint.settings.yml -o .
 else
     echo "Blueprint failed."
