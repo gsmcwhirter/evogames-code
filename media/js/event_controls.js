f2a013502bec4873ab248fdfde93ea08f3fc27a5
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-04 21:23:40 -0700

    Adding the ability to cancel events and removing various controls depending on event status

---
+++ media/js/event_controls.js
@@ -0,0 +1,68 @@
+$(function (){
+    var econtrols = $.sammy("#event-controls", function (){
+
+        this.bind('run', function (){
+            $("#remove-modal").overlay({
+                mask: {
+                    color: '#ccc',
+                    loadSpeed: 200,
+                    opacity: 0.9
+                },
+                top: 200,
+                closeOnClick: false
+            }).bind("open-overlay", function (){
+                var api = $(this).data("overlay");
+
+                api.load();
+            }).bind('close-overlay', function (){
+                var api = $(this).data("overlay");
+
+                api.close();
+            });
+        });
+
+        function noop(){}
+
+        this.get('', noop);
+        this.get("#!/", noop);
+        this.post("#!/", noop);
+
+        this.get("#!/cancel", function (){
+            $("#remove-modal").trigger("close-overlay");
+
+            this.redirect("#!/");
+        });
+
+        this.get("#!/remove", function (){
+            //show confirmation form
+            $("#remove-modal").trigger("open-overlay");
+        });
+
+        this.post("#!/remove", function (){
+            var self = this;
+
+            $.ajax({
+                type: 'delete',
+                url: "controls/delete",
+                dataType: 'json',
+                success: function (data){
+                    if (data.ok)
+                    {
+                        window.location = "/game";
+                    }
+                    else
+                    {
+                        $("#flash").trigger('error', [data.error]);
+                        self.redirect("#!/cancel");
+                    }
+                },
+                error: function (){
+                    $("#flash").trigger('error', ['Request error']);
+                    self.redirect("#!/cancel");
+                }
+            });
+        });
+    });
+
+    econtrols.run();
+});
