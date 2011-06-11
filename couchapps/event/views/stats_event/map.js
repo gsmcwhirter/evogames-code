8b1f9e4483a3e40f9aef40c74dd9d8799836584d
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-10 20:23:15 -0700

    Fixing some small issues

--- couchapps/event/views/stats_event/map.js
+++ couchapps/event/views/stats_event/map.js
@@ -2,7 +2,7 @@ function (doc){
     if (doc.type == "event-match"){
         doc.teams.forEach(function (team){
             team.players.forEach(function (player){
-                var stats = {handle: player.handle, alias: player.alias, groupcode: player.groupcode};
+                var stats = {handle: player.handle, alias: player.alias, groupcode: player.groupcode || false};
                 for (var statname in player.stats){
                     if (!isNaN(player.stats[statname])){
                         stats[statname] = player.stats[statname];
@@ -13,7 +13,7 @@ function (doc){
                     }
                 }
 
-                emit([doc.eventid, 0, player.handle.toLowerCase(), player.alias.toLowerCase(), player.groupcode.toLowerCase()], stats);
+                emit([doc.eventid, 0, player.handle.toLowerCase(), player.alias.toLowerCase(), (player.groupcode || "").toLowerCase()], stats);
             })
         });
     }
