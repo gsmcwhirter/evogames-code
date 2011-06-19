2725ab0e4e859bb08ebb8439f8c17cf5382ec5d6
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-19 12:49:03 -0700

    Fixing some views and view queries

--- couchapps/event/views/enddates_admin/map.js
+++ couchapps/event/views/enddates_admin/map.js
@@ -2,7 +2,7 @@ function (doc){
     if (doc.type == "event"){
         emit([doc.creator.toLowerCase(), doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "creator"});
         (doc.admins || []).forEach(function (admin){
-            emit([admin.toLowerCase(), doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "admin"});
+            emit([admin.toLowerCase(), 0, doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "admin"});
         });
     }
 }
--- couchapps/event/views/enddates_game/map.js
+++ couchapps/event/views/enddates_game/map.js
@@ -1,5 +1,5 @@
 function (doc){
     if (doc.type == "event"){
-        emit([doc.gameid, doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid});
+        emit([doc.gameid, 0, doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid});
     }
 }
