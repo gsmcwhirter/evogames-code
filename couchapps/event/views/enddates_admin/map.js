fb13ce4ffab2de94a0f1b4609d51e5120493b4f8
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-20 20:26:09 -0700

    Fixing the admin map for creators

--- couchapps/event/views/enddates_admin/map.js
+++ couchapps/event/views/enddates_admin/map.js
@@ -1,6 +1,6 @@
 function (doc){
     if (doc.type == "event"){
-        emit([doc.creator.toLowerCase(), doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "creator"});
+        emit([doc.creator.toLowerCase(), 0, doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "creator"});
         (doc.admins || []).forEach(function (admin){
             emit([admin.toLowerCase(), 0, doc.enddate ? doc.enddate : "N", 0, doc.startdate], {event: doc, _id: doc.gameid, admintype: "admin"});
         });
