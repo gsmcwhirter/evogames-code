95059d291e3b83729bc1cc13bf2930861735dff8
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-20 21:15:21 -0700

    Changing paths

--- socketserver.js
+++ socketserver.js
@@ -6,9 +6,9 @@ require('./socketapp')(ioserver);
 
 cluster('./app')
     .set('workers', 1)
-    .use(cluster.logger('/var/log/node/evogames'))
+    .use(cluster.logger('/var/log/node/evogames-socket'))
     .use(cluster.stats())
-    .use(cluster.pidfiles('/var/run/node/evogames'))
+    .use(cluster.pidfiles('/var/run/node/evogames-socket'))
     .use(cluster.cli())
     .use(cluster.repl(7091))
     .listen(7081);
